e3328b73bb97747fd9b57c3803a5c95a1d54e3ad
iortega-dev <iortegamancera@gmail.com>
2021-01-27 18:58:33 +0100

    feat: added 'should start 5 matches' test case

--- tests/scoreboard.test.ts
+++ tests/scoreboard.test.ts
@@ -14,6 +14,17 @@ describe('Scoreboard', () => {
       expect(matches).toBeDefined();
       expect(matches[0]).toBeUndefined();
     });
+
+    it('should start 5 matches', () => {
+      instance.startGame('Mexico', 'Canada');
+      instance.startGame('Spain', 'Brazil');
+      instance.startGame('Germany', 'France');
+      instance.startGame('Uruguay', 'Italy');
+      instance.startGame('Argentina', 'Australia');
+      const matches = instance.matches;
+      expect(matches).toBeDefined();
+      expect(matches.length).toBe(5);
+    });
   })
   
   describe('summary', () => {
