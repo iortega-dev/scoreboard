797158b2159e4dbbbcdae8a9a8ef9ae9c274b901
iortega-dev <iortegamancera@gmail.com>
2021-01-27 18:52:27 +0100

    feat: added new Scoreboard instance test

--- tests/scoreboard.test.ts
+++ tests/scoreboard.test.ts
@@ -1,6 +1,19 @@
+import Scoreboard from '../src/scoreboard';
+
 describe('Scoreboard', () => {
   describe('basic functionality', () => {
-    
+    let instance: Scoreboard;
+
+    beforeAll(() => {
+      instance = new Scoreboard();
+    });
+
+    it('should instance new Scoreboard with no matches', () => {
+      expect(instance).toBeInstanceOf(Scoreboard);
+      const matches = instance.matches;
+      expect(matches).toBeDefined();
+      expect(matches[0]).toBeUndefined();
+    });
   })
   
   describe('summary', () => {
